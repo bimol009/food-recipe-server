ea5bd0730c0c1a064f8c2db4f7bd5fddd0e82e06
bimol009 <110928529+bimol009@users.noreply.github.com>
2023-05-03 10:31:14 +0600

    module

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('gello World!')
-})
+app.use(cors())
+
+const categories = require("./data/category.json");
+const recipes = require("./data/food.json");
+
+app.get("/", (req, res) => {
+  res.send("gello World!");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/recipes", (req, res) => {
+    res.send(recipes);
+  });
+  
+  app.get("/recipes/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedNews = recipes.find((nw) => nw._id === id);
+    res.send(selectedNews);
+  });
+
+app.get("/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if(id===0){
+    res.send(recipes)
+  }
+
+  else{
+  const selectedCategory = recipes.filter(
+    (category) => parseInt(category.category_id) === id
+  );
+  res.send(selectedCategory);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
