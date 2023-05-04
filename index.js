b717c5abe1a4bf2aa46354fe42e293d16c7d7675
bimol009 <110928529+bimol009@users.noreply.github.com>
2023-05-04 15:13:57 +0600

    food json

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-
+app.use(cors());
 const categories = require("./data/category.json");
 const recipes = require("./data/food.json");
-
 
 app.get("/", (req, res) => {
   res.send("gello World!");
@@ -18,29 +16,23 @@ app.get("/categories", (req, res) => {
 });
 
 app.get("/recipes", (req, res) => {
-    res.send(recipes);
-  });
-  
-  app.get("/recipes/:id", (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    const selectedNews = recipes.find((nw) => nw._id === id);
-    res.send(selectedNews);
-  });
+  res.send(recipes);
+});
+
+app.get("/recipes/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedNews = recipes.find((nw) => nw._id === id);
+  res.send(selectedNews);
+});
 
 app.get("/categories/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
-  if(id===0){
-    res.send(recipes)
-  }
-
-  else{
-  const selectedCategory = recipes.filter(
+  const selectedCategory = recipes.find(
     (category) => parseInt(category.category_id) === id
   );
   res.send(selectedCategory);
-  }
 });
 
 app.listen(port, () => {
